d7c2e85813140e70d2636d80f9ab1e063c4195b0
Parikshit Hooda <phooda804@live.com>
2021-12-30 05:37:52 +0530

    Create jdb-basicio.java

---
+++ jdb-basicio.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		final double SHORTPI = 31.14159;
+		System.out.println("Hello world");
+		int var1=100, var2;
+		System.out.println(var1);
+	}
+}
